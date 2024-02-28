dc4e7895342889656343539d9f71926589edd81a
devetude <devetude@naver.com>
2024-02-28 14:42:38 +0900

    Implement the radix sort

---
+++ src/sort/RadixSort.kt
@@ -0,0 +1,28 @@
+package sort
+
+import java.util.LinkedList
+
+/**
+ * Time Complexity: O(dn)
+ * Space Complexity: O(n)
+ */
+fun main() {
+    val arr = intArrayOf(0, 1, 2, 21, 22, 34, 41, 47, 5, 6, 77, 79, 80, 81, 999).apply { shuffle() }
+    println(arr.joinToString())
+
+    val maxLen = 3
+    var buckets = Array(size = 10) { LinkedList<Int>() }
+    for (n in 1..maxLen) {
+        arr.forEach { buckets[it.digitOf(n)].add(it) }
+        var j = 0
+        buckets.forEach { it.forEach { num -> arr[j++] = num } }
+        buckets = Array(size = 10) { LinkedList<Int>() }
+    }
+    println(arr.joinToString())
+}
+
+fun Int.digitOf(n: Int): Int {
+    require(1 <= n)
+    val div = generateSequence(seed = 1) { it * 10 }.take(n).last()
+    return this / div % 10
+}
