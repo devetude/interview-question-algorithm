861b9b97cac442b4a6ed7fe2f945f190ea22c508
devetude <devetude@naver.com>
2024-03-17 12:57:03 +0900

    Fix the kruskal algorithm

--- src/graph/KruskalAlgorithm.kt
+++ src/graph/KruskalAlgorithm.kt
@@ -1,5 +1,7 @@
 package graph
 
+private val parentOf: IntArray = IntArray(size = 6) { it }
+
 /**
  * Time Complexity:
  * - Union-Find: O(eloge) (e = edge)
@@ -10,9 +12,9 @@ package graph
 fun main() {
     val edges = mutableListOf(
         Triple(first = 1, second = 3, third = 3),
+        Triple(first = 1, second = 2, third = 4),
         Triple(first = 1, second = 4, third = 3),
         Triple(first = 2, second = 5, third = 8),
-        Triple(first = 3, second = 1, third = 4),
         Triple(first = 3, second = 4, third = 3),
         Triple(first = 3, second = 5, third = 5),
         Triple(first = 4, second = 5, third = 6)
@@ -20,26 +22,33 @@ fun main() {
     println("edges=${edges.joinToString()}")
 
     val selectedEdges = mutableListOf<Triple<Int, Int, Int>>()
-    for (i in 0 until edges.lastIndex) {
-        val (u, v, w) = edges[i]
-        union()
-        if (find(u) == find(v)) continue
+    for (i in edges.indices) {
+        val (u, v, _) = edges[i]
+        if (isCycleCreatedBy(u, v)) continue
 
         selectedEdges.add(edges[i])
+        if (selectedEdges.size == edges.size - 1) break
     }
     println("selectedEdges=${selectedEdges.joinToString()}")
+
+    val minWeightSum = selectedEdges.sumOf { it.third }
+    println(minWeightSum)
 }
 
 private fun MutableList<Triple<Int, Int, Int>>.sortByWeight() = sortWith(
     compareBy<Triple<Int, Int, Int>> { it.third }.thenBy { it.first }.thenBy { it.second }
 )
 
-private fun union() {
-    // TODO: Implement here.
+private fun isCycleCreatedBy(u: Int, v: Int): Boolean {
+    val parentOfU = find(u)
+    val parentOfV = find(v)
+    if (parentOfU == parentOfV) return true
+
+    parentOf[v] = parentOfU
+
+    return false
 }
 
-private fun find(u: Int): Int {
-    // TODO: Implement here.
-    return -1
-}
+private fun find(u: Int): Int =
+    if (parentOf[u] == u) u else find(parentOf[u]).also { parentOf[u] = it }
 
